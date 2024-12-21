c27700500f1a192aa270a13efb5797b244388ab5
Seonghyun Park <shp4rk@gmail.com>
2024-12-21 15:37:41 +0900

    day19 part 2

--- src/bin/day19.rs
+++ src/bin/day19.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 const INPUT: &str = include_str!("./day19.txt");
 
@@ -7,26 +7,35 @@ struct Patterns<'a>(Vec<&'a str>);
 
 struct Parser<'a, 'b> {
     patterns: Patterns<'a>,
-    cache: HashSet<&'b str>,
+    cache: HashMap<&'b str, usize>,
 }
 
 impl<'a, 'b> Parser<'a, 'b> {
-    fn try_parse(&mut self, input: &'b str) -> Result<(), &'static str> {
-        if self.patterns.0.contains(&input) {
-            self.cache.insert(input);
-            return Ok(());
+    fn warm_up(&mut self) {
+        self.cache.insert("", 1);
+    }
+
+    fn try_parse(&mut self, input: &'b str) -> Result<usize, &'static str> {
+        if let Some(&k) = self.cache.get(input) {
+            return Ok(k);
         }
+
+        let mut num_representations = 0usize;
 
         for &pattern in self.patterns.0.clone().iter() {
             if input.starts_with(pattern) {
-                if let Ok(_) = self.try_parse(&input[pattern.len()..]) {
-                    self.cache.insert(input);
-                    return Ok(())
+                if let Ok(k) = self.try_parse(&input[pattern.len()..]) {
+                    num_representations += k;
                 }
             }
         }
 
-        Err("meh")
+        if num_representations > 0 {
+            self.cache.insert(input, num_representations);
+            Ok(num_representations)
+        } else {
+            Err("meh")
+        }
     }
 }
 
@@ -35,12 +44,23 @@ fn main() {
 
     let mut parser = Parser {
         patterns: Patterns(parts[0].split(", ").collect::<Vec<_>>()),
-        cache: HashSet::new(),
+        cache: HashMap::new(),
     };
 
-    let res = parts[1].lines()
-        .filter(|input|  parser.try_parse(input).is_ok())
+    parser.warm_up();
+
+    let inputs = parts[1].lines().collect::<Vec<_>>();
+
+    let res = inputs.iter()
+        .filter(|input| parser.try_parse(input).is_ok())
         .count();
+
+    println!("{}", res);
+
+    let res = parser.cache.iter().filter_map(|(k, &v)| {
+        if inputs.contains(k) { Some(v) } else { None }
+    })
+    .sum::<usize>();
 
     println!("{}", res);
 }
